add all gradient backgrounds fixed minor bug

base_model.py
@@ -35,7 +35,7 @@ class base_model():
         # generate colors
 
         # backgound
-        self.b = [(224,224,224),(0,128,255),(153,0,153),(0,0,0)] #[grey, blue, purple, rainbow]
+        self.b = (0,0,0)
         # outline
         self.o = (0,0,0) # always black
         # white
@@ -53,43 +53,7 @@ class base_model():
         self.h,self.h_actual,self.h_closet = self.set_part_color()
         #...
 
-        self.bg = Image.open('test_bg/bg.png')
-        #self.bg = np.array(bg)
-        #self.bg = cv2.imread('test_bg/bg.png')
 
-        red = (244,67,54)
-        org = (255,152,0)
-        yel = (255,235,59)
-        gre = (204,255,144)
-        dgr = (0,150,136)
-        lbl = (0,188,212)
-        blu = (33,150,243)
-        pur = (103,58,183)
-
-        self.rainbow = [[red,red,red,red,red,org,org,org,org,org,org,yel,yel,yel,yel,yel,yel,yel,gre,gre,gre,gre,gre,gre], 
-                        [red,red,red,red,org,org,org,org,org,org,yel,yel,yel,yel,yel,yel,yel,gre,gre,gre,gre,gre,gre,dgr], 
-                        [red,red,red,org,org,org,org,org,org,yel,yel,yel,yel,yel,yel,yel,gre,gre,gre,gre,gre,gre,dgr,dgr], 
-                        [red,red,org,org,org,org,org,org,yel,yel,yel,yel,yel,yel,yel,gre,gre,gre,gre,gre,gre,dgr,dgr,dgr], 
-                        [red,org,org,org,org,org,org,yel,yel,yel,yel,yel,yel,yel,gre,gre,gre,gre,gre,gre,dgr,dgr,dgr,dgr], 
-                        [org,org,org,org,org,org,yel,yel,yel,yel,yel,yel,yel,gre,gre,gre,gre,gre,gre,dgr,dgr,dgr,dgr,dgr], 
-                        [org,org,org,org,org,yel,yel,yel,yel,yel,yel,yel,gre,gre,gre,gre,gre,gre,dgr,dgr,dgr,dgr,dgr,dgr], 
-                        [org,org,org,org,yel,yel,yel,yel,yel,yel,yel,gre,gre,gre,gre,gre,gre,dgr,dgr,dgr,dgr,dgr,dgr,lbl], 
-                        [org,org,org,yel,yel,yel,yel,yel,yel,yel,gre,gre,gre,gre,gre,gre,dgr,dgr,dgr,dgr,dgr,dgr,lbl,lbl], 
-                        [org,org,yel,yel,yel,yel,yel,yel,yel,gre,gre,gre,gre,gre,gre,dgr,dgr,dgr,dgr,dgr,dgr,lbl,lbl,lbl], 
-                        [org,yel,yel,yel,yel,yel,yel,yel,gre,gre,gre,gre,gre,gre,dgr,dgr,dgr,dgr,dgr,dgr,lbl,lbl,lbl,lbl], 
-                        [yel,yel,yel,yel,yel,yel,yel,gre,gre,gre,gre,gre,gre,dgr,dgr,dgr,dgr,dgr,dgr,lbl,lbl,lbl,lbl,lbl], 
-                        [yel,yel,yel,yel,yel,yel,gre,gre,gre,gre,gre,gre,dgr,dgr,dgr,dgr,dgr,dgr,lbl,lbl,lbl,lbl,lbl,blu], 
-                        [yel,yel,yel,yel,yel,gre,gre,gre,gre,gre,gre,dgr,dgr,dgr,dgr,dgr,dgr,lbl,lbl,lbl,lbl,lbl,blu,blu], 
-                        [yel,yel,yel,yel,gre,gre,gre,gre,gre,gre,dgr,dgr,dgr,dgr,dgr,dgr,lbl,lbl,lbl,lbl,lbl,blu,blu,blu], 
-                        [yel,yel,yel,gre,gre,gre,gre,gre,gre,dgr,dgr,dgr,dgr,dgr,dgr,lbl,lbl,lbl,lbl,lbl,blu,blu,blu,blu], 
-                        [yel,yel,gre,gre,gre,gre,gre,gre,dgr,dgr,dgr,dgr,dgr,dgr,lbl,lbl,lbl,lbl,lbl,blu,blu,blu,blu,blu], 
-                        [yel,gre,gre,gre,gre,gre,gre,dgr,dgr,dgr,dgr,dgr,dgr,lbl,lbl,lbl,lbl,lbl,blu,blu,blu,blu,blu,blu], 
-                        [gre,gre,gre,gre,gre,gre,dgr,dgr,dgr,dgr,dgr,dgr,lbl,lbl,lbl,lbl,lbl,blu,blu,blu,blu,blu,blu,pur], 
-                        [gre,gre,gre,gre,gre,dgr,dgr,dgr,dgr,dgr,dgr,lbl,lbl,lbl,lbl,lbl,blu,blu,blu,blu,blu,blu,pur,pur], 
-                        [gre,gre,gre,gre,dgr,dgr,dgr,dgr,dgr,dgr,lbl,lbl,lbl,lbl,lbl,blu,blu,blu,blu,blu,blu,pur,pur,pur], 
-                        [gre,gre,gre,dgr,dgr,dgr,dgr,dgr,dgr,lbl,lbl,lbl,lbl,lbl,blu,blu,blu,blu,blu,blu,pur,pur,pur,pur], 
-                        [gre,gre,dgr,dgr,dgr,dgr,dgr,dgr,lbl,lbl,lbl,lbl,lbl,blu,blu,blu,blu,blu,blu,pur,pur,pur,pur,pur], 
-                        [gre,dgr,dgr,dgr,dgr,dgr,dgr,lbl,lbl,lbl,lbl,lbl,blu,blu,blu,blu,blu,blu,pur,pur,pur,pur,pur,pur]]
 
         # Common: one of 1-3
         # Rare: Two of 1-3
@@ -103,8 +67,8 @@ class base_model():
     def choose_tier(self,tier):
 
         if tier == 'common':
-            self.b = self.b[0]
-
+            self.bg = Image.open('backgrounds/common.png')
+        
             if randint(0,1) == 0:
                 self.e = self.e[0]
             else:
@@ -114,21 +78,24 @@ class base_model():
             # choose accessories
 
         elif tier == 'rare':
-            self.b = self.b[1]
+            self.bg = Image.open('backgrounds/rare.png')
+       
             if randint(0,1) == 0:
                 self.e = self.e[0]
             else:
                 self.e = self.e[1]
 
         elif tier == 'epic':
-            self.b = self.b[2]
+            self.bg = Image.open('backgrounds/epic.png')
+        
             if randint(0,1) == 0:
                 self.e = self.e[0]
             else:
                 self.e = self.e[1]
 
         elif tier == 'legendary':
-            self.b = self.b[3]
+
+            self.bg = Image.open('backgrounds/legendary.png')
             if randint(0,1) == 0:
                 self.e = self.e[0]
             else: